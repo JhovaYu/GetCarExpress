c79b2daa5d4d7f3bd9f063efc4c9f30e31ea2614
DavvvL <david.levet74@unach.mx>
2025-03-12 11:52:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+window.addEventListener('scroll', function() {
+  const title = document.getElementById('title');
+  const header = document.getElementById('header');
+  const content = document.getElementById('content');
+  
+  // Control de visibilidad y animaciones
+  if (window.scrollY > 50) {
+    // Mover el título hacia arriba y hacerlo invisible
+    title.style.position = 'fixed';
+    title.style.top = '20px';
+    title.style.left = '50%';
+    title.style.transform = 'translateX(-50%)';
+    title.style.fontSize = '5rem'; // Reducir tamaño del título
+    title.style.opacity = '0'; // Desaparecer el título
+
+    // Mostrar el header (moverlo hacia abajo desde la parte superior)
+    header.style.display = 'block'; // Aseguramos que el header se muestre
+    header.style.opacity = '1'; // Hacerlo visible
+    header.style.transform = 'translateY(0)'; // Deslizar desde arriba
+    header2.style.transform = 'translateY(-1080%)';
+    header2.style.padding = '8px';
+
+    // Animar el contenido
+    content.classList.add('show');
+  } else {
+    header2.style.display = 'block'; // Aseguramos que el header se muestre
+    header2.style.opacity = '1'; // Hacerlo visible
+    header2.style.transform = 'translateY(0)';
+    // Restaurar el título a su estado original
+    title.style.position = 'absolute';
+    title.style.top = '50%';
+    title.style.transform = 'translate(-50%, -50%)';
+    title.style.fontSize = '13rem';
+    title.style.opacity = '1'; // Volver a mostrar el título
+
+    // Ocultar el header (deslizar hacia arriba)
+    header.style.opacity = '0'; // Hacerlo invisible
+    header.style.transform = 'translateY(-100%)'; // Deslizar fuera de la vista
+    header2.style.padding = '20px';
+    
+    // Ocultar el contenido
+    content.classList.remove('show');
+  }
+});
